5d145e7630f96b3f8df0aa7fee81eb75b792e8b4
Topskys <2477350343@qq.com>
2025-08-09 19:54:20 +0800

    ci(commitlint): 使用git-zc统一生成标准化的Git提交消息

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  // unit git-cz
+  prompt: {
+    useEmoji: true,
+  },
+};
